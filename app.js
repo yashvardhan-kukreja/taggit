002a69ba9d2d408193db3c5ca42d7edd7614a7f9
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-10-02 06:57:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var app = express();
+var nodemailer = require("nodemailer");
+var path = require("path");
+var port = process.env.PORT || 7040;
+
+app.get('/', (req, res) => {
+	
+	res.sendFile(path.join(__dirname + '/index.html'));
+
+});
+
+app.get('/sendmail', (req, res) => {
+	
+var smtp = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: 'tagg.it.18@gmail.com',
+                        pass: 'taggitrocks'
+                    }
+                });
+                var mailOptions = {
+                    to: "goelashwin36@gmail.com",
+                    from: 'tagg.it.18@gmail.com',
+                    subject: 'TaggIt Notification!!!',
+                    html: '<h1>Kapde chori hone vaale hai!!!!</h1>'
+                };
+
+                smtp.sendMail(mailOptions, function (err) {
+                    if (err) {
+                        console.log(err);
+                        console.log("Error sending the mail");
+                    } else {
+                        console.log("Mail sent");
+                    }
+
+});
+});
+
+
+app.listen(port, () => {
+    console.log("App running successfully on port number: " + port + "..")
+});
