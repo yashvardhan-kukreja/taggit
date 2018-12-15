43cce201ee53c5d91d0eec812aed9c422b892689
Yashvardhan-Kukreja <yash.kukreja.98@gmail.com>
2018-12-15 09:24:27 +0530

    removed API usages

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 var express = require("express");
 var app = express();
 var nodemailer = require("nodemailer");
-var sgtransport = require('nodemailer-sendgrid-transport');
 var path = require("path");
 var port = process.env.PORT || 3294;
 
-var sendgrid_username = process.env.SENDGRID_USERNAME;
-var sendgrid_password = process.env.SENDGRID_PASSWORD;
-
 
 app.get('/:email', (req, res) => {
-    var options = {
-        auth: {
-            api_user: sendgrid_username,
-            api_key: sendgrid_password
-        }
-    };
 
+    var smtp = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'tagg.it.18@gmail.com',
+            pass: 'taggitrocks'
+        }
+    });
     var mailOptions = {
         to: req.params.email,
         from: 'tagg.it.18@gmail.com',
         subject: 'TaggIt Notification!!!',
         html: '<h1>ALERT! ALERT! <br> Clothes are being stolen</h1>'
     };
-    let client = nodemailer.createTransport(sgtransport(options));
 
-    client.sendMail(mailOptions, (err) => {
+    smtp.sendMail(mailOptions, function (err) {
         if (err) {
-            console.error(err);
-            res.json({success: false, message: "Error occurred while sending the mail"});
+            console.log(err);
+            console.log("Error sending the mail");
+            res.json({success: false, message: "Problem sending the email"});
         } else {
+            console.log("Mail sent");
             res.json({success: true, message: "Mail sent successfully"});
         }
+
     });
 });
+
 
 app.listen(port, () => {
     console.log("App running successfully on port number: " + port + "..")
